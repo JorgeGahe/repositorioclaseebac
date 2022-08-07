f9152ad426ab872a4e5992ee411ff7c2fa6a8945
Jorge Gallegos <jorge_x_2000@hotmail.com>
2022-08-07 18:25:46 -0500

    Practica Modulo 6 Corregida
    -Se modifico la Terea con los comentarios del Tutor
    -dejar de hacer cubos con programación y tomar los objetos directos para realizar la Tarea

--- ProyectoInicialEBAC/Assets/Modulo6/Script/ScOnDE.cs
+++ ProyectoInicialEBAC/Assets/Modulo6/Script/ScOnDE.cs
@@ -4,39 +4,13 @@ using UnityEngine;
 
 public class ScOnDE : MonoBehaviour
 {
-    GameObject cuboOnDE;
-    
-    Vector3[] vertices = {
-        new Vector3 (0 ,0 ,0), //vertice0
-        new Vector3 (1 ,0 ,0), //vertice1
-        new Vector3 (1 ,1 ,0), //vertice2
-        new Vector3 (0 ,1 ,0), //vertice3
-        new Vector3 (0 ,1 ,1), //vertice4
-        new Vector3 (1 ,1 ,1), //vertice5
-        new Vector3 (1 ,0 ,1), //vertice6
-        new Vector3 (0 ,0 ,1), //vertice7
-
-       
-    };
-
-    int[] triangulos = {
-        0, 2, 1, //Cara 1
-        0, 3, 2,
-        2, 3, 4, //Cara 2
-        2, 4, 5,
-        1, 2, 5, //Cara 3
-        1, 5, 6,
-        0, 7, 4, //Cara 4
-        0, 4, 3,
-        5, 4, 7, //Cara 5
-        5, 7, 6,
-        0, 6, 7, //Cara 6
-        0, 1, 6,
-    };
+    public GameObject Prefabcubo;
+    public List<GameObject> listaDeCubos;
+    public int numCubos = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        listaDeCubos = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -47,39 +21,15 @@ public class ScOnDE : MonoBehaviour
 
     private void OnEnable()
     {
-        cuboOnDE = new GameObject("Nuestro Primer Cubo");
-        cuboOnDE.AddComponent<MeshFilter>();
-        var meshFilter = cuboOnDE.GetComponent<MeshFilter>().mesh;
-        meshFilter.Clear();
-        meshFilter.vertices = vertices;
-        meshFilter.triangles = triangulos;
-        meshFilter.Optimize();
-        meshFilter.RecalculateNormals();
-        cuboOnDE.AddComponent<BoxCollider>();
-        var boxCollider = cuboOnDE.GetComponent<BoxCollider>();
-        boxCollider.center = new Vector3(0.5f, 0.5f, 0.5f);
-        cuboOnDE.AddComponent<MeshRenderer>();
-        var meshRendermaterial = cuboOnDE.GetComponent<MeshRenderer>().material;
-        meshRendermaterial.color = Color.red;
-        cuboOnDE.transform.position = Vector3.right;
+        numCubos++;
+        GameObject tempGameObject = Instantiate<GameObject>(Prefabcubo);
+        tempGameObject.name = "CuboNumero" + numCubos;
     }
 
     private void OnDisable()
     {
-        cuboOnDE = new GameObject("Nuestro Primer Cubo");
-        cuboOnDE.AddComponent<MeshFilter>();
-        var meshFilter = cuboOnDE.GetComponent<MeshFilter>().mesh;
-        meshFilter.Clear();
-        meshFilter.vertices = vertices;
-        meshFilter.triangles = triangulos;
-        meshFilter.Optimize();
-        meshFilter.RecalculateNormals();
-        cuboOnDE.AddComponent<BoxCollider>();
-        var boxCollider = cuboOnDE.GetComponent<BoxCollider>();
-        boxCollider.center = new Vector3(0.5f, 0.5f, 0.5f);
-        cuboOnDE.AddComponent<MeshRenderer>();
-        var meshRendermaterial = cuboOnDE.GetComponent<MeshRenderer>().material;
-        meshRendermaterial.color = Color.blue;
-        cuboOnDE.transform.position = Vector3.up;
+        numCubos++;
+        GameObject tempGameObject = Instantiate<GameObject>(Prefabcubo);
+        tempGameObject.name = "CuboNumero" + numCubos;
     }
 }
--- ProyectoInicialEBAC/Assets/Modulo6/Script/ScUpdate.cs
+++ ProyectoInicialEBAC/Assets/Modulo6/Script/ScUpdate.cs
@@ -4,60 +4,20 @@ using UnityEngine;
 
 public class ScUpdate : MonoBehaviour
 {
-    GameObject cuboUpdate;
+    public GameObject Prefabcubo;
+    public List<GameObject> listaDeCubos;
     public int numCubos = 0;
-
-    Vector3[] vertices = {
-        new Vector3 (0 ,0 ,0), //vertice0
-        new Vector3 (1 ,0 ,0), //vertice1
-        new Vector3 (1 ,1 ,0), //vertice2
-        new Vector3 (0 ,1 ,0), //vertice3
-        new Vector3 (0 ,1 ,1), //vertice4
-        new Vector3 (1 ,1 ,1), //vertice5
-        new Vector3 (1 ,0 ,1), //vertice6
-        new Vector3 (0 ,0 ,1), //vertice7
-
-       
-    };
-
-    int[] triangulos = {
-        0, 2, 1, //Cara 1
-        0, 3, 2,
-        2, 3, 4, //Cara 2
-        2, 4, 5,
-        1, 2, 5, //Cara 3
-        1, 5, 6,
-        0, 7, 4, //Cara 4
-        0, 4, 3,
-        5, 4, 7, //Cara 5
-        5, 7, 6,
-        0, 6, 7, //Cara 6
-        0, 1, 6,
-    };
     // Start is called before the first frame update
     void Start()
     {
-        
+        listaDeCubos = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        cuboUpdate = new GameObject("Nuestro Primer Cubo");
-        cuboUpdate.AddComponent<MeshFilter>();
-        var meshFilter = cuboUpdate.GetComponent<MeshFilter>().mesh;
-        meshFilter.Clear();
-        meshFilter.vertices = vertices;
-        meshFilter.triangles = triangulos;
-        meshFilter.Optimize();
-        meshFilter.RecalculateNormals();
-        cuboUpdate.AddComponent<BoxCollider>();
-        var boxCollider = cuboUpdate.GetComponent<BoxCollider>();
-        boxCollider.center = new Vector3(0.5f, 0.5f, 0.5f);
-        cuboUpdate.AddComponent<MeshRenderer>();
-        var meshRendermaterial = cuboUpdate.GetComponent<MeshRenderer>().material;
-        meshRendermaterial.color = Color.black;
-        cuboUpdate.transform.position = Vector3.zero;
         numCubos++;
+        GameObject tempGameObject = Instantiate<GameObject>(Prefabcubo);
+        tempGameObject.name = "CuboNumero" + numCubos;
     }
 }
